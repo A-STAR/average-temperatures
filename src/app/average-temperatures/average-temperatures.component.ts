313f2606c7537cd0d0c20acf3826f355b0f18e62
A-STAR <andreysuperstar@gmail.com>
2018-02-17 21:22:12 +0300

    feat(average-temperatures): add Temperature Map interface

--- src/app/average-temperatures/average-temperatures.component.ts
+++ src/app/average-temperatures/average-temperatures.component.ts
@@ -2,12 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 import { AverageTemperaturesService } from './shared/average-temperatures.service';
 
+import { TemperatureMap } from './shared/temperature-map.interface';
+
 @Component({
   selector: 'app-average-temperatures',
   templateUrl: './average-temperatures.component.html',
   styleUrls: ['./average-temperatures.component.sass']
 })
 export class AverageTemperaturesComponent implements OnInit {
+
+  temperatureMaps: TemperatureMap[];
 
   constructor(private averageTemperatures: AverageTemperaturesService) { }
 
---
+++ src/app/average-temperatures/shared/temperature-map.interface.ts
@@ -0,0 +1,4 @@
+export interface TemperatureMap {
+  temperature?: number;
+  color: string;
+}
