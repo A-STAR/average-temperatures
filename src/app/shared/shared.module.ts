7037a8b6a15c2e535c8ac7fcd9b6f3baaa3d9e5c
A-STAR <andreysuperstar@gmail.com>
2018-02-19 01:06:50 +0300

    chore(shared): add ReactiveFormsModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { TemperatureMapComponent } from './temperature-map/temperature-map.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   declarations: [TemperatureMapComponent],
   exports: [
     CommonModule,
