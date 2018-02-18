17ecc19a270230b545be3dfd95b38ca67b28b1a7
A-STAR <andreysuperstar@gmail.com>
2018-02-19 00:26:38 +0300

    feat: add markup and logic to Tempeture Map component, print maps

--- src/app/shared/temperature-map/temperature-map.component.ts
+++ src/app/shared/temperature-map/temperature-map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnChanges, OnInit, Input } from '@angular/core';
 
 import { TemperatureMap } from '../';
 
@@ -7,14 +7,42 @@ import { TemperatureMap } from '../';
   templateUrl: './temperature-map.component.html',
   styleUrls: ['./temperature-map.component.sass']
 })
-export class TemperatureMapComponent implements OnInit {
+export class TemperatureMapComponent implements OnChanges, OnInit {
 
   @Input() temperatureMap: TemperatureMap;
+  @Input() i: number;
+
+  gradientId: string;
+  progressFill: string;
+
+  mapWidth = 1000;
+  mapHeight = 30;
+  rectHeight: number;
+  progressIndent: number;
+  progress: number;
+  pointerWidth = 2;
+  trianglePoints: string;
+  triangle = true;
 
   constructor() { }
 
+  ngOnChanges() {
+    if (this.temperatureMap.temperature === undefined) {
+      this.temperatureMap.temperature = 50;
+    }
+
+    this.rectHeight = this.mapHeight - 7;
+
+    this.progress = this.mapWidth / 100 * this.temperatureMap.temperature;
+    this.trianglePoints = `${this.progress - 7} ${this.mapHeight},
+                   ${this.progress} ${this.mapHeight - 7},
+                   ${this.progress + 7} ${this.mapHeight}`;
+    this.progressIndent = this.triangle ? this.mapHeight : this.rectHeight;
+  }
+
   ngOnInit() {
-    this.temperatureMap.temperature = 50;
+    this.gradientId = `gradient${this.i}`;
+    this.progressFill = `url(#${this.gradientId})`;
   }
 
 }
