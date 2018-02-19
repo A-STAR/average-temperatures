375b05f1f0135d0066dd80950b472ef51c4d229e
A-STAR <andreysuperstar@gmail.com>
2018-02-19 18:49:31 +0300

    chore(temperature-map): complete logic & styles

--- src/app/shared/temperature-map/temperature-map.component.ts
+++ src/app/shared/temperature-map/temperature-map.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnChanges, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { debounceTime } from 'rxjs/operators/debounceTime';
+import { distinctUntilChanged } from 'rxjs/operators/distinctUntilChanged';
 
 import { TemperatureMap } from '../';
 
@@ -15,8 +19,8 @@ export class TemperatureMapComponent implements OnChanges, OnInit {
   gradientId: string;
   progressFill: string;
 
-  mapWidth = 1000;
-  mapHeight = 30;
+  mapWidth = 425;
+  mapHeight = 32;
   rectHeight: number;
   progressIndent: number;
   progress: number;
@@ -24,25 +28,88 @@ export class TemperatureMapComponent implements OnChanges, OnInit {
   trianglePoints: string;
   triangle = true;
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnChanges() {
     // if (this.temperatureMap.temperature === undefined) {
     //   this.temperatureMap.temperature = 50;
     // }
 
-    this.rectHeight = this.mapHeight - 7;
-
-    this.progress = this.mapWidth / 100 * this.temperatureMap.temperature;
-    this.trianglePoints = `${this.progress - 7} ${this.mapHeight},
-                   ${this.progress} ${this.mapHeight - 7},
-                   ${this.progress + 7} ${this.mapHeight}`;
-    this.progressIndent = this.triangle ? this.mapHeight : this.rectHeight;
+    this.update();
   }
 
   ngOnInit() {
     this.gradientId = `gradient${this.i}`;
     this.progressFill = `url(#${this.gradientId})`;
+
+    this.form = this.fb.group({
+      width: [
+        {
+          value: this.mapWidth,
+          disabled: false
+        },
+        {
+          validators: [
+            Validators.min(100),
+            Validators.max(1000)
+          ],
+          asyncValidators: [],
+          updateOn: 'change'
+        }
+      ],
+      height: [
+        this.mapHeight,
+        [
+          Validators.min(5),
+          Validators.max(30)
+        ]
+      ],
+      progress: [
+        this.temperatureMap.temperature,
+        [
+          Validators.min(0),
+          Validators.min(100)
+        ]
+      ],
+      pointer: [
+        this.pointerWidth,
+        [
+          Validators.min(0.01),
+          Validators.min(5)
+        ]
+      ],
+      triangle: this.triangle
+    });
+
+    this.form.valueChanges
+    .pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+      )
+      .subscribe(this.update.bind(this));
+      // .subscribe(console.log);
+  }
+
+  // tslint:disable-next-line:max-line-length
+  update({ width, height, progress, pointer, triangle } = { width: this.mapWidth, height: this.mapHeight, progress: this.temperatureMap.temperature, pointer: this.pointerWidth, triangle: this.triangle }) {
+
+    this.mapWidth = width;
+    this.mapHeight = height;
+    this.temperatureMap.temperature = progress;
+    this.pointerWidth = pointer;
+    this.triangle = triangle;
+
+    this.progress = this.mapWidth / 100 * this.temperatureMap.temperature;
+
+    this.rectHeight = this.mapHeight - 7;
+
+    this.trianglePoints = `${this.progress - 7} ${this.mapHeight},
+                           ${this.progress} ${this.mapHeight - 7},
+                           ${this.progress + 7} ${this.mapHeight}`;
+
+    this.progressIndent = this.triangle ? this.mapHeight : this.rectHeight;
   }
 
 }
